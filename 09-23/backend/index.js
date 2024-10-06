ba7f70291aac8790f4a070eb1f699cceee5a479c
Eerik Poopuu <91658707+Eerikpoopuu@users.noreply.github.com>
2024-10-06 13:57:49 +0300

    feat:token

--- 09-23/backend/index.js
+++ 09-23/backend/index.js
@@ -1,25 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const morgan = require("morgan");
+const jwt = require("jsonwebtoken"); 
 const port = 8080;
+
+const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 
+
 const catsRoutes = require("./routes/cats.routes");
 const exampleRoutes = require("./routes/example.routes");
 const todoRoutes = require("./routes/todo.routes");
-app.use(express.json());
 
 app.use("/cats", catsRoutes);
 app.use("/examples", exampleRoutes);
 app.use("/todo", todoRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+
+
+app.post("/token", (req, res) => {
+    const name = req.body.name; 
+ 
+    const token = jwt.sign({ name }, "shhh");
+    res.send({ token });
+});
+
+
+app.post("/verify", (req, res) => {
+    const token = req.body.token; 
+
+    jwt.verify(token, "shhh", (err, decoded) => {
+       
+        res.send("Token is valid"); 
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
