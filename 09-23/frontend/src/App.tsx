f84182eaceb2258a1b1ded30c3a54a5fcec5da07
Eerik Poopuu <91658707+Eerikpoopuu@users.noreply.github.com>
2024-10-06 11:51:33 +0300

    feat:theme

--- 09-23/frontend/src/App.tsx
+++ 09-23/frontend/src/App.tsx
@@ -1,14 +1,36 @@
 import "./App.css";
 import Cats from "./componets/Cats";
 import Todos from "./componets/todos";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#D5006D', 
+    },
+    secondary: {
+      main: '#000000', 
+    },
+    background: {
+      default: '#000000', 
+      paper: '#212121', 
+    },
+    text: {
+      primary: '#FFFFFF', 
+      secondary: '#D5006D',
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <Cats />
-      <Todos/>
-    </>
+      <ThemeProvider theme={theme}>
+        <Cats />
+        <Todos />
+      </ThemeProvider>
   );
 }
 
 export default App;
+
